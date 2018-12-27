0bfb8b90d7e35e85d8c9bb14df4e6b7afad58094
Ian Huang <ian.huang@dyn-160-39-132-248.dyn.columbia.edu>
2018-12-27 08:24:11 +0800

    setup of dataloade

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,4 +1,30 @@
+"""
+@author : Ian Huang
+The dataloader for the Salcon dataset
+"""
+import os 
+from scipy.misc import imread
+import numpy as np
+import pickle # for loading semantic labels
+
+
 class SalconDataloader(object):
-    def __init__(self.b
+    def __init__(self, rgb_csv, sal_csv, data_csv):
+        pass
+    
+    def get_class_freq(self):
+        pass
+    
+    def get_label2class_conv(self):
+        pass
 
+    def set_desired_labels(self):
+        pass
+    
+    def __iter__(self, random=True):
+        """ gets a random sample if random=True, or ordered down the list if False
+        """
+        pass
 
+if __name__=='__main__':
+    pass
