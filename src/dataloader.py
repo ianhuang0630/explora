cd8190a683bca126d2935a930afcaaf8c425ff3f
Ian Huang <ian.huang@dyn-160-39-132-248.dyn.columbia.edu>
2018-12-29 14:44:17 +0800

    fixes

--- src/dataloader.py
+++ src/dataloader.py
@@ -7,10 +7,11 @@ from scipy.misc import imread
 import numpy as np
 import pandas as pd
 import pickle # for loading semantic labels
+from tqdm import tqdm
 
 class SalconDataloader(object):
     def __init__(self, data_path_csv, sem_id_csv, gt_type='pix', 
-                 random_draw=True, classes=[]):
+                 random_draw=True, classes=[], verbose=True):
         """ Constructor for the Salcon dataset
         Input:
             rgb_csv (str): path to rgb data csv
@@ -21,36 +22,44 @@ class SalconDataloader(object):
             random_draw (optional, bool): true if the draw of every sample is
                 random, false if it goes down the list.
             classes (optional, list): list of classes that will be extracted
-                from the dataset. 
+                from the dataset.
+            verbose (optional, bool): True if verbose, false otherwise.
         """
         self.data_path_csv = data_path_csv
         self.sem_id_csv = sem_id_csv
         self.gt_type = gt_type
         assert self.gt_type == 'pix' or self.gt_type == 'bbox'
-        
+        self.verbose = verbose
+
         self.random_draw = random_draw
         self.classes = classes
         # making label2id sem2id 
+        if self.verbose:
+            print('reading semantic id mapping csv...')
         label2id_pd = pd.read_csv(self.sem_id_csv)
-        sem2id = {}
+        self.sem2id = {}
+        self.id2sem = {}
+        if self.verbose:
+            print('constructing semantic and id mappings...')
         for idx, element in label2id_pd.iterrows():
-            sem2id[element['name']] = element['id']
-        assert all([element in sem2id for element in self.classes]),\
+            self.sem2id[element['name']] = element['id']
+            self.id2sem[element['id']] = element['name']
+        assert all([element in self.sem2id for element in self.classes]),\
                 'Elements of self.classes needs to be {}'.\
-                format(list(sem2id.values()))
+                format(list(self.sem2id.values()))
         # conversion from self.classes to id's
-        self.focus_ids = set([sem2id[element] for element in self.classes])
+        self.focus_ids = set([self.sem2id[element] for element in self.classes])
         if len(self.focus_ids) == 0: # if empty:
-            self.focus_ids = set(list(sem2id.values())) # include everything
+            self.focus_ids = set(list(self.sem2id.values())) # include everything
         
         # reading csv
         self.data_path = pd.read_csv(self.data_path_csv)
         # preprocess the classes to hold only required classes
         self.available_indices = self.filter_by_classes()
    
-    def get_label2class_conv(self, id_num):
+    def get_id2class_conv(self, id_num):
         pass 
-    def get_class2label_conv(self, class_str):
+    def get_class2id_conv(self, class_str):
         pass
 
     def set_desired_labels(self):
@@ -62,19 +71,26 @@ class SalconDataloader(object):
             indices (list): a list of row indices in self.data_path where 
                             conditions hold.
         """
-        indices = [] 
-        for index, row in self.data_path.iterrows():
-            path = row['semantic_label']
-            assert os.path.exists(path), \
-                'sematic_label path {} does not exist'.format(path)
-            assert '.pkl' in path, 'Expected {} to be a pkl file'.format(path)
+        # if less than all categories are in target_id's:
+        if len(self.focus_ids) < len(self.sem2id):
+            indices = [] 
+            for index, row in tqdm(self.data_path.iterrows()):
+                path = row['semantic_label']
+                assert os.path.exists(path), \
+                    'sematic_label path {} does not exist'.format(path)
+                assert '.pkl' in path, 'Expected {} to be a pkl file'.format(path)
+                
+                with open(path, 'rb') as f:
+                    labels = pickle.load(f)
+                    if any([element in labels.flatten() for element in \
+                            list(self.focus_ids)]):
+                        # save index
+                        indices.append(index)
+            # TODO save indices at a given location
+        
+        else: # all categories are kept
+            indices = list(range(len(self.data_path)))
             
-            with open(path, 'rb') as f:
-                labels = pickle.load(f)
-                if any([element in labels.flatten() for element in \
-                        list(self.focus_ids)]):
-                    # save index
-                    indices.append(index)
         return indices 
 
     def __iter__(self):
@@ -93,10 +109,13 @@ class SalconDataloader(object):
             self.read_index = random.rand_int(0, len(self.available_indices)-1)
             prod = self.data_path.iloc(self.available_indices[self.read_index])
         else:
-            #increment read_index  
-            prod = self.data_path.iloc(self.available_indices[self.read_index])
-            self.read_index += 1
-        
+            #increment read_index
+            if self.read_index <= len(self.available_indices)-1:
+                prod = self.data_path.iloc(self.available_indices[self.read_index])
+                self.read_index += 1
+            else:
+                raise StopIteration
+
         rgb = imread(prod['rgb'])
         gaze = imread(prod['gaze'])
         with open(prod['semantic_label'], 'rb') as f:
