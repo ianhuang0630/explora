27c2bf7559a9e8259e9a8ca69eb86ae891a6a6d4
Ian Huang <ian.huang@dyn-160-39-132-248.dyn.columbia.edu>
2018-12-31 15:22:35 +0800

    dataloader and utils after following pytorch tutorial

--- src/dataloader.py
+++ src/dataloader.py
@@ -10,12 +10,159 @@ import pickle # for loading semantic labels
 from tqdm import tqdm
 from multiprocessing import Pool
 import random
+from torch.utils.data import Dataset, Dataloader
 
-class SalconDataloader(object):
+class SalconDataset(Dataset):
+    """This is a Dataset object that needs to passed through Dataloader with
+    an appropriately specified batchsize, shuffle/no shuffle ...etc
+    """
+    def __init__(self, data_path_csv, sem_id_csv, train=True, transform=None, 
+                 gt_type='pix', classes=[], , read_cache=True, verbose=True):
+        """ Initializer for the Salcon dataset
+        Input:
+            data_path_csv (str): csv with path to training instances
+            sem_id_csv (str): path to semantic metadata csv
+            train (bool): True if dataloader is used for training
+            gt_type (optional, str): ground truth type
+            classes (optional, list): list of classes that will be extracted
+                from the dataset.
+            read_cache (optional, bool): True if the indices for a certain 
+                set of classes is to be read from memory. False if the indices
+                must be recomputed and resaved.
+            verbose (optional, bool): True if verbose, false otherwise.
+        """
+        self.data_path_csv = data_path_csv
+        self.sem_id_csv = sem_id_csv
+        self.gt_type = gt_type
+        assert self.gt_type == 'pix' or self.gt_type == 'bbox'
+        self.verbose = verbose
+        self.train = train
+        self.read_cache = read_cache
+        self.transform = transform
+
+        self.classes = classes
+        # making label2id sem2id 
+        if self.verbose:
+            print('reading semantic id mapping csv...')
+        label2id_pd = pd.read_csv(self.sem_id_csv)
+        self.sem2id = {}
+        self.id2sem = {}
+        if self.verbose:
+            print('constructing semantic and id mappings...')
+        for idx, element in label2id_pd.iterrows():
+            self.sem2id[element['name']] = element['id']
+            self.id2sem[element['id']] = element['name']
+        assert all([element in self.sem2id for element in self.classes]),\
+                'Elements of self.classes needs to be {}'.\
+                format(list(self.sem2id.keys()))
+        # conversion from self.classes to id's
+        if self.verbose:
+            print('getting focus classes ids...')
+        self.focus_ids = set([self.sem2id[element] for element in self.classes])
+        if len(self.focus_ids) == 0: # if empty:
+            self.focus_ids = set(list(self.sem2id.values())) # include everything
+        if self.verbose:
+            print('loading data path csv...')
+        # reading csv
+        self.data_path = pd.read_csv(self.data_path_csv)
+        # preprocess the classes to hold only required classes
+        if self.verbose:
+            print('Constructing list of indices in csv satisfying class requirements...')
+        self.available_indices = self.filter_by_classes()
+   
+    def get_id2class_conv(self, id_num):
+        return self.id2sem[id_num]
+
+    def get_class2id_conv(self, class_str):
+        return self.sem2id[class_str] 
+
+    def filter_by_classes(self):
+        """ find images with at least one instance self.focus_ids in sem labels
+        Returns:
+            indices (list): a list of row indices in self.data_path where 
+                            conditions hold.
+        """
+        # if less than all categories are in target_id's:
+        # creating name -- .[train/val]_[alphabetized classes]_indices.npy
+        # under directory ../directory
+        if self.train:
+            filename = 'train_'+\
+                        '+'.join(['_'.join(element.split(' ')) \
+                                for element in sorted(self.classes)])+\
+                        '_indices.npy'
+        else:
+            filename = 'val_'+\
+                        '+'.join(['_'.join(element.split(' ')) \
+                                for element in sorted(self.classes)])+\
+                        '_indices.npy'
+
+        save_path = os.path.join(os.path.dirname(self.data_path_csv),
+                                filename)
+
+        if len(self.focus_ids) < len(self.sem2id):
+            if self.read_cache and os.path.exists(save_path):
+                if self.verbose:
+                    print('reading {}'.format(save_path)) 
+                indices = np.load(save_path).tolist()
+            else: # create and save indices 
+                indices = [] 
+                for index, row in tqdm(self.data_path.iterrows()):
+                    path = row['semantic_label']
+                    # some checks
+                    assert os.path.exists(path), \
+                        'semantic_label path {} does not exist'.format(path)
+                    assert '.pkl' in path, 'Expected {} to be a pkl file'.\
+                            format(path)
+                    # checking if desired labels are in current dataset 
+                    with open(path, 'rb') as f:
+                        labels = pickle.load(f)
+                        if any([element in labels.flatten() for element in \
+                                list(self.focus_ids)]):
+                            # save index
+                            indices.append(index)
+                # save indices at a given location
+                if self.verbose:
+                    print('saving to cache file {}'.format(save_path))
+                np.save(save_path, np.array(indices))
+                if self.verbose:
+                    print('saved.')
+
+        else: # all categories are kept, and no need to be saved
+            indices = list(range(len(self.data_path)))
+        return indices 
+    
+    def __len_(self):
+        return len(self.data_path)
+    
+    def __getitem__(self, idx):
+        prod = self.data_path.iloc[idx]
+        rgb = imread(element['rgb'])
+        gaze = imread(element['gaze'])
+        with open(element['semantic_label'], 'rb') as f:
+            sem_label = pickle.load(f)
+        
+        # zeroing out the classes not in the selected classes 
+        target = np.zeros_like(sem_label)
+        for cat in list(self.focus_ids):
+            vert, horiz = np.where(sem_label == cat)
+            target[vert, horiz] = cat
+        
+        sample = {'rgb': rgb, 'gaze': gaze, 'target': target}
+        if self.gt_type == 'bbox':
+            # TODO: yet to be implemented
+            raise ValueError('bbox gt not yet implemented')
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+        
+
+class SalconDataloaderInHouse(object):
+    """ Implementation of a dataloader that is Pytorch independent
+    """
     def __init__(self, data_path_csv, sem_id_csv, gt_type='pix', 
                  random_draw=True, classes=[], train=True, read_cache=True, 
                  batch_size=1, verbose=True):
-        """ Constructor for the Salcon dataset
+        """ Initializer for the Salcon dataset
         Input:
             data_path_csv (str): csv with path to training instances
             sem_id_csv (str): path to semantic metadata csv
@@ -80,9 +227,6 @@ class SalconDataloader(object):
     def get_class2id_conv(self, class_str):
         return self.sem2id[class_str] 
 
-    def set_desired_labels(self, new_classes):
-        pass
-    
     def filter_by_classes(self):
         """ find images with at least one instance self.focus_ids in sem labels
         Returns:
@@ -187,4 +331,4 @@ class SalconDataloader(object):
 
 
 if __name__=='__main__':
-    saldata = SalconDataloader('../data/train_rgb_gaze_sem.csv', '../data/semid.csv')
+    saldata = SalconDataloaderInHouse('../data/train_rgb_gaze_sem.csv', '../data/semid.csv')
---
+++ src/utils.py
@@ -0,0 +1,93 @@
+"""
+@author: Ian Huang
+"""
+import torch
+import numpy as np
+from skimage import io, transform
+
+class Rescale(object):
+    def __init__(self, output_size, target_type='pix'):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+        assert target_type=='pix' or target_type=='bbox'
+        self.target_type = target_type
+
+    def __call__(self, sample):
+       	image, gaze, target = sample['rgb'], sample['gaze'], sample['target']
+         
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+        gz = transform.resize(gaze, (new_h, new_w))
+        if self.target_type == 'pix':
+            tg = transform.resize(target)
+        else:
+            raise ValueError('Havent yet implemented bounding box ground truth')
+            # TODO: implement this
+            # landmarks = landmarks * [new_w / w, new_h / h]
+            # h and w are swapped for landmarks because for images,
+            # x and y axes are axis 1 and 0 respectively
+
+        return {'rgb': img, 'gaze': gz, 'target': tg} 
+
+class RandomCrop(object):
+    def __init__(self, output_size, target_type='pix'):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size 
+        
+        assert target_type=='pix' or target_type=='bbox'
+        self.target_type = target_type
+
+    def __call__(self, sample):
+	image, gaze, target = sample['rgb'], sample['gaze'], sample['target']
+        
+	h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        img = image[top: top + new_h,
+                left: left + new_w, :]
+        gz = gaze [top:top+new_h, left:left+new_w]
+        if self.target_type == 'pix':
+            tg = target[top: top+new_h, left:left+new_w]
+        else:
+            raise ValueError('Havent yet implemented bounding box ground truth')
+            # TODO: implement this
+        return {'rgb': img, 'gaze': gz, 'target': tg}
+
+class ToTensor(object):
+    def __init__(self, target_type='pix'):
+        assert target_type=='pix' or target_type=='bbox'
+        self.target_type = target_type
+
+    def __call__(self, sample):
+        image, gaze, target = sample['rgb'], sample['gaze'], sample['target']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        img = torch.from_numpy(image)
+        gz = torch.from_numpy(gaze)
+        if self.target_type=='pix':
+            tg = torch.from_numpy(target)
+        else:
+            raise ValueError('Havent yet implemented bounding box ground truth')
+            # TODO: implement this
+        return {'image': img, 'gaze': gz, 'target': tg} 
+
